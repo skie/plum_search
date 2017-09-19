63b20431c92788e16d1b5e6bfd1c2c1358031772
Yevgeny Tomenko <skie@mail.ru>
2017-09-20 00:42:07 +0300

    introduce FormParameter::isEmpty method

--- src/FormParameter/BaseParameter.php
+++ src/FormParameter/BaseParameter.php
@@ -171,6 +171,11 @@ abstract class BaseParameter
         return $this->value;
     }
 
+    public function isEmpty()
+    {
+        return $this->value === null || is_string($this->value) && $this->value == '';
+    }
+
     public function __toString()
     {
         return (string)$this->value;
--- src/View/Helper/SearchHelper.php
+++ src/View/Helper/SearchHelper.php
@@ -104,7 +104,7 @@ class SearchHelper extends Helper
     protected function _setValue(&$input, $param)
     {
         $value = $param->value();
-        if (!empty($value)) {
+        if (!$param->isEmpty()) {
             $input['value'] = $value;
         }
 
